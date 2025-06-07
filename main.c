f1c647751fbcd843863de861199228b8f43b9121
Huy Vu <vuquanghuy2k@gmail.com>
2025-06-07 21:44:25 +0700

    Add chunk - Update write byte - Update header file for chunk

--- main.c
+++ main.c
@@ -4,11 +4,13 @@
 int main(int argc, const char* argv[]) {
     Chunk chunk;
     new_chunk(&chunk);
-	int constant = add_constant(&chunk, 1.2);
 
 	write_byte_to_chunk(&chunk, OP_CONSTANT, 123);
+	
+	int constant = add_constant(&chunk, 1.2);
 	write_byte_to_chunk(&chunk, constant, 123);
-    write_byte_to_chunk(&chunk, OP_RETURN, 123);
+    
+	write_byte_to_chunk(&chunk, OP_RETURN, 123);
 	
     disassemble_chunk(&chunk, "test chunk");
     free_chunk(&chunk);
