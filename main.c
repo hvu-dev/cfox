a041d50644b18febfc4b8ed36ac9590ed0fd1863
Huy Vu (John) <64062575+hvu-dev@users.noreply.github.com>
2025-07-31 14:49:38 +0700

    Update read file and run file functions

--- main.c
+++ main.c
@@ -21,19 +21,42 @@ static void start_repl() {
   }
 }
 
-static char *run_file(const char *file_path) {
+static char *read_file(const char *file_path) {
   FILE *file = fopen(file_path, "rb");
-  fseek(file, 0L, SEEK_END);
 
+  if(file == NULL) {
+    fprintf(stderr, "Could not open file '%s'", file_path);
+    exit(74);
+  }
+
+  fseek(file, 0L, SEEK_END);
   size_t file_size = ftell(file);
   rewind(file);
+
   char *buffer = (char*) malloc(file_size + 1);
+  if(buffer == NULL) {
+    fprintf(stderr, "Memory is not sufficient to read file '%s'", file_path);
+    exit(74);
+  }
+
   size_t bytes_read = fread(buffer, sizeof(char), file_size, file);
+  if(bytes_read < file_size) {
+    fprintf(stderr, "Could not read file '%s'", file_path);
+    exit(74);
+  }
   buffer[bytes_read] = '\0';
 
   fclose(file);
 
   return buffer;
+}
+
+static void run_file(const char *file_path){
+  char *source = read_file(file_path);
+  InterpretResult result = interpret(source);
+
+  if(result == INTERPRETER_COMPILE_ERROR) exit(65);
+  if(result == INTERPRETER_RUNTIME_ERROR) exit(70);
 }
 
 int main(int argc, const char *argv[]) {
